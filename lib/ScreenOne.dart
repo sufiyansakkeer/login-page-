86ed31fab132b2c40cc9c43362f0bc2e6a9c9f22
sufiyan <sufiyansakkeer616@gmail.com>
2022-12-17 12:45:40 +0530

    designed login page ,added snackbar and alertbox

--- lib/ScreenOne.dart
+++ lib/ScreenOne.dart
@@ -14,35 +14,88 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: ListView(children: <Widget>[
-          Container(
-            alignment: Alignment.center,
-            padding: const EdgeInsets.all(20),
-            child: Text('user name'),
-          ),
-          Container(
-            alignment: Alignment.center,
-            padding: const EdgeInsets.all(20),
-            child: TextFormField(
-              controller: _nameofuser,
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: ListView(children: <Widget>[
+            const SizedBox(
+              height: 80,
             ),
-          ),
-          Container(
-            alignment: Alignment.center,
-            padding: const EdgeInsets.all(20),
-            child: TextFormField(
-              controller: _passwordofuser,
-            ),
-          ),
-          Container(
+            Container(
               alignment: Alignment.center,
-              padding: const EdgeInsets.all(20),
-              child: ElevatedButton(
-                child: Text('login'),
-                onPressed: () {},
-              )),
-        ]),
+              // padding: const EdgeInsets.all(20),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'User Name',
+                ),
+                controller: _nameofuser,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              alignment: Alignment.center,
+              // padding: const EdgeInsets.all(20),
+              child: TextFormField(
+                obscureText: true,
+                controller: _passwordofuser,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Password',
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+                alignment: Alignment.bottomRight,
+                // padding: const EdgeInsets.all(0),
+                child: ElevatedButton(
+                  child: const Text('login'),
+                  onPressed: () {
+                    checking(context);
+                  },
+                )),
+          ]),
+        ),
       ),
     );
+  }
+
+  void checking(BuildContext ctx) {
+    final _username = _nameofuser.text;
+    final _password = _passwordofuser.text;
+
+    if (_username == _password) {
+      //go to home
+    } else {
+      final _errormessage = 'username and passsword does not match';
+      //snackbar
+      ScaffoldMessenger.of(ctx).showSnackBar(SnackBar(
+        behavior: SnackBarBehavior.floating,
+        backgroundColor: Colors.red,
+        content: Text(_errormessage),
+      ));
+
+      //alert dialogue
+
+      showDialog(
+          context: ctx,
+          builder: (context1) {
+            return AlertDialog(
+              title: Text('dude ..'),
+              content: Text(_errormessage),
+              actions: [
+                TextButton(
+                    onPressed: () {
+                      Navigator.of(context1).pop();
+                    },
+                    child: Text('Got It'))
+              ],
+            );
+          });
+    }
   }
 }
